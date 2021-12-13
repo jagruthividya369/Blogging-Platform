a4470a10d6d4209bdfae21bec097cfdda22318a2
Ajith B <ajith.b@tavant.com>
2021-12-13 17:19:40 +0530

    first commit

--- Online Blogging Platform/src/main/java/com/project/demo/controllers/UserController.java
+++ Online Blogging Platform/src/main/java/com/project/demo/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
